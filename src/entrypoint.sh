fe9d07aa2e58b195afe84c63c184e0825ebcc81e
Urmzd Mukhammadnaim <urmzd@outlook.com>
2022-12-08 15:57:46 -0500

    build: change example

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -11,6 +11,7 @@ echo "Commiting with message: $COMMIT_MESSAGE"
 git config user.name "embed-md bot"
 git config user.email "embed-md-bot@urmzd.com"
 
+git pull --rebase
 git add "$FILES"
 git commit -m "$COMMIT_MESSAGE"
 git push
