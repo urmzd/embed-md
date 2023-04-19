1e42c18419da37ab09c9da8f31b560bfc1464a24
Urmzd Mukhammadnaim <urmzd@dal.ca>
2023-04-19 11:44:07 -0300

    fix: always push to current branch

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,12 +3,6 @@
 COMMIT_MESSAGE="$1"
 FILES="${@:2}"
 
-# Check if inside a git repository
-if ! git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
-  echo "Not in a git repository. Please run the script inside a valid git repository."
-  exit 1
-fi
-
 # Configure git
 git config user.name "embed-md bot"
 git config user.email "embed-md-bot@urmzd.com"
@@ -22,7 +16,13 @@ npx embedme "$FILES"
 
 # Commit changes
 echo "Commiting with message: $COMMIT_MESSAGE"
+
+# Pull and apply changes
 git pull
+
+# Stage and commit the changes
 git add "$FILES"
 git commit -m "$COMMIT_MESSAGE"
+
+# Push the changes to the current branch
 git push
