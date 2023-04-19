ff405c33b0c50e6e0d8a0ef98a482360ae22b369
Urmzd Mukhammadnaim <urmzd@dal.ca>
2023-04-19 11:35:18 -0300

    fix: ensure current directory is considered safe

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,15 +3,26 @@
 COMMIT_MESSAGE="$1"
 FILES="${@:2}"
 
+# Check if inside a git repository
+if ! git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
+  echo "Not in a git repository. Please run the script inside a valid git repository."
+  exit 1
+fi
+
+# Configure git
+git config user.name "embed-md bot"
+git config user.email "embed-md-bot@urmzd.com"
+git config --add safe.directory "$(git rev-parse --show-toplevel)"
+git config pull.rebase true
+git config rebase.autoStash true
+
+# Embed files
 echo "Embedding files: $FILES"
 npx embedme "$FILES"
 
+# Commit changes
 echo "Commiting with message: $COMMIT_MESSAGE"
-
-git config user.name "embed-md bot"
-git config user.email "embed-md-bot@urmzd.com"
-
-git pull --rebase
+git pull
 git add "$FILES"
 git commit -m "$COMMIT_MESSAGE"
 git push
