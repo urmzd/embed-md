0baef5e0642e9829a146fab3f3c2a7731d7897c4
Urmzd <urmzd@dal.ca>
2024-01-12 18:55:28 -0500

    fix: permission errors

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 _commit_changes() {
   git add "${MARKDOWN_FILES[@]}"
-  git commit -m "$COMMIT_MESSAGE" --author="$COMMIT_AUTHOR"
+  git commit -m "$COMMIT_MESSAGE"
 }
 
 _embed_files() {
@@ -13,10 +13,14 @@ _push_changes() {
   git push
 }
 
-main () {
-  # Ensure that /github/workspace is allowed.
+_cfg() {
   git config --global --add safe.directory /github/workspace
+  git config --global user.email "$COMMIT_EMAIL"
+  git config --global user.name "$COMMIT_NAME"
+}
 
+main () {
+  _cfg
   _embed_files
   _commit_changes
 
